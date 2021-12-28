b9eafe485e7c45e90bebcc5e6d4393d785cb1cbc
omeranha <53233626+omeranha@users.noreply.github.com>
2021-12-27 21:18:44 -0300

    someday, who knows...

--- src/main/java/meranha/mekatana/MekaWeapons.java
+++ src/main/java/meranha/mekatana/MekaWeapons.java
@@ -1,38 +1,16 @@
 package meranha.mekatana;
 
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(MekaWeapons.MOD_ID)
 public class MekaWeapons {
 
     public static final String MOD_ID = "mekaweapons";
-    public static IProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
 
     public MekaWeapons() {
         ModConfig.registerConfigs(ModLoadingContext.get());
         Items.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        this.preInit(event);
-        this.init(event);
-        this.postInit(event);
-    }
-
-    private void preInit(FMLCommonSetupEvent event) {
-        proxy.preInit(event);
-    }
-
-    private void init(FMLCommonSetupEvent event) {
-        proxy.init(event);
-    }
-
-    private void postInit(FMLCommonSetupEvent event) {
-        proxy.postInit(event);
     }
 }
